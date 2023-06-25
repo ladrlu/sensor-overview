d7d4c15631f003747ebee77dca0f96dbef601f20
Lucie Ládrová <lucieladrova@appio.cz>
2023-06-25 09:30:30 +0200

    Add core libraries for the project

---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#00a179",
+      light: "#33b393",
+      dark: "#007054",
+    },
+    secondary: {
+      main: "#EBA607",
+      light: "#efb738",
+      dark: "#a47404",
+    },
+  },
+});
