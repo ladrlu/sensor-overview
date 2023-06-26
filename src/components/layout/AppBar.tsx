2ecba61e8b34e96a85c08321354e75545e2ffbe5
Lucie Ládrová <lucieladrova@appio.cz>
2023-06-26 16:40:38 +0200

    Udate AppBar

--- src/components/layout/AppBar.tsx
+++ src/components/layout/AppBar.tsx
@@ -1,18 +1,29 @@
 import AppBarMUI from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import FmdGoodIcon from "@mui/icons-material/FmdGood";
+import { theme } from "../../theme";
+import { Link } from "react-router-dom";
+import LinkMUI from "@mui/material/Link";
 
 export default function AppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBarMUI position="static">
         <Toolbar variant="dense">
-          <FmdGoodIcon sx={{ mr: 2 }} />
-          <Typography variant="h6" component="div">
-            Sensor Overview
-          </Typography>
+          <LinkMUI
+            to="/"
+            variant="h6"
+            component={Link}
+            sx={{
+              color: theme.palette.primary.contrastText,
+              textDecoration: "none",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <FmdGoodIcon sx={{ mr: 2 }} /> Sensor Overview
+          </LinkMUI>
         </Toolbar>
       </AppBarMUI>
     </Box>
